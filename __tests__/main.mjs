3ee8754071f28ab0cf60ad02dfb9cf410467dd0d
Roee Shapira <roee.shapira@tensorleap.ai>
2023-02-27 12:34:43 +0200

    Add tests

--- __tests__/main.mjs
+++ __tests__/main.mjs
@@ -12,11 +12,19 @@ function parse(source) {
 }
 
 describe("Extens TS Ops Plugin", () => {
-	test("Array literal + array literal", () => {
+	test("Empty array literal + empty array literal", () => {
 		const { code } = parse("[] + [];");
 		expect(code).toEqual("[].concat([]);");
 	});
-	test("Identifier += array literal", () => {
+	test("Array literal + array literal", () => {
+		const { code } = parse("[1, 2, 3] + [4];");
+		expect(code).toEqual("[1, 2, 3].concat([4]);");
+	});
+	test("Empty object literal + empty object literal", () => {
+		const { code } = parse("{} + {};");
+		expect(code).toEqual("Object.assign({}, {});");
+	});
+	test("Identifier += empty array literal", () => {
 		const { code } = parse("const a = []; a += [];");
 		expect(code).toEqual("const a = []; a.push(...[]);");
 	});
